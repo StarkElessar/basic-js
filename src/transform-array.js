ad0efb3eb907e46a6b07bf3f9aef95c2ff5039f4
Stark <serj.elessar@gmail.com>
2022-10-15 17:59:25 +0300

    feat: transform array  complete

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,50 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  const params = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev',
+  ]
+  const newCorrectArray = [...arr]
+
+  newCorrectArray.forEach((item, i) => {
+    if (params.includes(item)) {
+      if (item === params[0]) {
+        newCorrectArray[i] = undefined
+        newCorrectArray[i + 1] = undefined
+      } else if (item === params[1]) {
+        newCorrectArray[i] = undefined
+        newCorrectArray[i - 1] = undefined
+      } else if (item === params[2]) {
+        newCorrectArray[i] = newCorrectArray[i + 1]
+      } else if (item === params[3]) {
+        newCorrectArray[i] = newCorrectArray[i - 1]
+      }
+    }
+  })
+
+  return newCorrectArray.filter((item) => item !== undefined)
 }
 
 module.exports = {
-  transform
-};
+  transform,
+}
