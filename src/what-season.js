11ddefa170940d76b828839f158b845478bc9937
Stark <serj.elessar@gmail.com>
2022-10-15 16:22:27 +0300

    refactor: refactoring getSeason func

--- src/what-season.js
+++ src/what-season.js
@@ -12,25 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js')
  *
  */
 function getSeason(date) {
-  const err = 'Invalid date!'
-
-  if (!date) return 'Unable to determine the time of year!'
-  if (!(date instanceof Date)) throw new Error(err)
-
   try {
-    date.getYear()
-    const seasonObj = {
-      winter: [0, 1, 11],
-      spring: [2, 3, 4],
-      summer: [5, 6, 7],
-      autumn: [8, 9, 10],
-    }
-  
-    for (let key in seasonObj) {
-      if(seasonObj[key].includes(date.getMonth())) return key
+    if (!date) {
+      return 'Unable to determine the time of year!'
+    } else {
+      date.getYear()
+      const seasonObj = {
+        winter: [0, 1, 11],
+        spring: [2, 3, 4],
+        summer: [5, 6, 7],
+        autumn: [8, 9, 10],
+      }
+
+      for (let key in seasonObj) {
+        if (seasonObj[key].includes(date.getMonth())) return key
+      }
     }
   } catch {
-    throw new Error(err)
+    throw new Error('Invalid date!')
   }
 }
 
