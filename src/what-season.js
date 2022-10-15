097e7ad88e00a1c3005956c909952ebd8c1703cd
Stark <serj.elessar@gmail.com>
2022-10-15 16:18:43 +0300

    refactor: refactoring getSeason func

--- src/what-season.js
+++ src/what-season.js
@@ -13,34 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js')
  */
 function getSeason(date) {
   const err = 'Invalid date!'
+
   if (!date) return 'Unable to determine the time of year!'
   if (!(date instanceof Date)) throw new Error(err)
 
   try {
     date.getYear()
-  } catch (e) {
-    console.log(e)
+    const seasonObj = {
+      winter: [0, 1, 11],
+      spring: [2, 3, 4],
+      summer: [5, 6, 7],
+      autumn: [8, 9, 10],
+    }
+  
+    for (let key in seasonObj) {
+      if(seasonObj[key].includes(date.getMonth())) return key
+    }
+  } catch {
     throw new Error(err)
-  }
-
-  const month = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', ]
-  const winter = [month[0], month[1], month[11]]
-  const spring = [month[2], month[3], month[4]]
-  const summer = [month[5], month[6], month[7]]
-  const autumn = [month[8], month[9], month[10]]
-
-  const currentMonth = month[date.getMonth()]
-
-  if (winter.includes(currentMonth)) {
-    return 'winter'
-  } else if (spring.includes(currentMonth)) {
-    return 'spring'
-  } else if (summer.includes(currentMonth)) {
-    return 'summer'
-  } else if (autumn.includes(currentMonth)) {
-    return 'autumn'
-  } else {
-    return err
   }
 }
 
